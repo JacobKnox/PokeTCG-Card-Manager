576359e99f1b0280853310c22e4fbd9c5bda2765
Jacob Knox <jacobknoxa@gmail.com>
2022-11-02 16:09:06 -0400

    Started Eloquent framework for site
    Created a few more layouts to generalize information on the website; Eloquent models for collections, cards, and decks; and controllers and migrations for the models.

--- laravelport/resources/views/decks.blade.php
+++ laravelport/resources/views/decks.blade.php
@@ -1,28 +1,6 @@
 <x-layout>
     <div id="decksdisplay">
-        <div class="deck">
-            <a href="decks.html" class="deckLink">
-                <button class="deckButton">
-                    <img src="https://images.pokemontcg.io/swsh4/25.png" alt="Charizard" class="cardPreview">
-                    <img src="https://images.pokemontcg.io/swsh4/25.png" alt="Charizard" class="cardPreview">
-                    <img src="https://images.pokemontcg.io/swsh4/25.png" alt="Charizard" class="cardPreview">
-                    <h2 class="deckName">My Deck</h2>
-                    <p>60 cards</p>
-                    <p class="deckDescription">This is a deck of all of my cards.</p>
-                </button>
-            </a>
-        </div>
-        <div class="deck">
-            <a href="decks.html" class="deckLink">
-                <button class="deckButton">
-                    <img src="https://images.pokemontcg.io/swsh4/25.png" alt="Charizard" class="cardPreview">
-                    <img src="https://images.pokemontcg.io/swsh4/25.png" alt="Charizard" class="cardPreview">
-                    <img src="https://images.pokemontcg.io/swsh4/25.png" alt="Charizard" class="cardPreview">
-                    <h2 class="deckName">Another Deck</h2>
-                    <p>60 cards</p>
-                    <p class="deckDescription">This is a deck of all of my cards.</p>
-                </button>
-            </a>
-        </div>
+        <x-deckpreview></x-deckpreview>
+        <x-deckpreview></x-deckpreview>
     </div>
 </x-layout>
