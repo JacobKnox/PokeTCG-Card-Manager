f99ac6668ef71196b3835a2f2ef521f0d1f5ed4b
Jacob Knox <jacobknoxa@gmail.com>
2023-02-12 23:28:13 -0500

    Starting CollectionController

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\DeckController;
+use App\Http\Controllers\CollectionController;
+use App\Http\Controllers\PokemonController;
 
 Route::get('register', [UserController::class, 'create']);
 
@@ -15,3 +18,7 @@ Route::resource('users', UserController::class)->except([
     'create',
     'store'
 ]);
+
+Route::resource('decks', DeckController::class);
+
+Route::resource('collections', CollectionController::class);
